1d68cf42999a3cebca48b697b2763d0d42868e82
Eric Kinsley <dt1993@gmail.com>
2020-03-28 12:17:27 -0500

    update js with counter

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,6 @@
 $("#start").on("click", function() {
-    for(var i=0;i<questions.length;i++){
-        $("#subwrapper").append("<h2>"+questions[i].question+"</h2");
-        for(var j=0;j<questions[i].answers.length;j++){
-            $("#subwrapper").append("<input type= 'radio' name='question-"+i+"' value='"+questions[i].answers[j]+"'>"+questions[i].answers[j]) 
-        }
-    }
+    game.start();
 })
-
 
 var questions = [{
     question: "Who won the World Cup in 2010?",
@@ -41,3 +35,28 @@ var questions = [{
     answers: ["Polo", "Pool", "Lacrosse", "Golf"],
     correctAnswer: "Polo"
 }];
+
+var game = {
+    correct: 0,
+    incorrect: 0,
+    counter: 120,
+    countdown: function(){
+        game.counter--;
+        $("#counter").html(game.counter);
+        if(game.counter<=0){
+            console.log("Time is up!");
+            game.done();
+        }
+    },
+    start: function(){
+        timer = setInterval(game.countdown,1000);
+        $("#subwrapper").prepend('<h2>Time Remaining: <span id="counter">120</span> Seconds</h2>')
+        $("#start").remove();
+        for(var i=0;i<questions.length;i++){
+            $("#subwrapper").append("<h2>"+questions[i].question+"</h2");
+            for(var j=0;j<questions[i].answers.length;j++){
+                $("#subwrapper").append("<input type= 'radio' name='question-"+i+"' value='"+questions[i].answers[j]+"'>"+questions[i].answers[j]) 
+            }   
+    }
+}
+}
