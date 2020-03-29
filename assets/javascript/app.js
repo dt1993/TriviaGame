bb94cbdaac1fe2e1ff44b173e02ede1396971d3f
Eric Kinsley <dt1993@gmail.com>
2020-03-29 11:55:05 -0500

    update css and js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,9 @@
-$("#start").on("click", function() {
+$('#start').on('click', function() {
     game.start();
+})
+
+$(document).on('click', '#end', function(){
+    game.done();
 })
 
 var questions = [{
@@ -39,24 +43,97 @@ var questions = [{
 var game = {
     correct: 0,
     incorrect: 0,
-    counter: 120,
+    counter: 5,
     countdown: function(){
         game.counter--;
-        $("#counter").html(game.counter);
-        if(game.counter<=0){
+        $('#counter').html(game.counter);
+        if(game.counter==0){
             console.log("Time is up!");
             game.done();
         }
     },
+
     start: function(){
         timer = setInterval(game.countdown,1000);
-        $("#subwrapper").prepend('<h2>Time Remaining: <span id="counter">120</span> Seconds</h2>')
+        $('#subwrapper').prepend('<h2>Time Remaining: <span id="counter">120</span> Seconds</h2>')
         $("#start").remove();
         for(var i=0;i<questions.length;i++){
-            $("#subwrapper").append("<h2>"+questions[i].question+"</h2");
+            $('#subwrapper').append('<h2>'+questions[i].question+'</h2');
             for(var j=0;j<questions[i].answers.length;j++){
                 $("#subwrapper").append("<input type= 'radio' name='question-"+i+"' value='"+questions[i].answers[j]+"'>"+questions[i].answers[j]) 
             }   
+        }
+        $('#subwrapper').append('<br><button id="end">Done</button>')    
+    },
+
+
+    done: function(){
+        $.each($("input[name='question-0']: checked"),function(){
+            if($(this).val()==questions[0].correctAnswer){
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        });
+        $.each($("input[name='question-1']: checked"),function(){
+            if($(this).val()==questions[1].correctAnswer){
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        });
+        $.each($("input[name='question-2']: checked"),function(){
+            if($(this).val()==questions[2].correctAnswer){
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        });
+        $.each($("input[name='question-3']: checked"),function(){
+            if($(this).val()==questions[3].correctAnswer){
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        });
+        $.each($("input[name='question-4']: checked"),function(){
+            if($(this).val()==questions[4].correctAnswer){
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        });
+        $.each($("input[name='question-5']: checked"),function(){
+            if($(this).val()==questions[5].correctAnswer){
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        });
+        $.each($("input[name='question-6']: checked"),function(){
+            if($(this).val()==questions[6].correctAnswer){
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        });
+        $.each($("input[name='question-7']: checked"),function(){
+            if($(this).val()==questions[7].correctAnswer){
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        });
+
+        this.result();
+    },
+
+    result: function(){
+        clearInterval(timer);
+        $('#subwrapper h2').remove();
+        $('#subwrapper').html("<h2>All done!</h2>");
+        $('#subwrapper').append("<h3>Correct Answers: "+this.correct+"</h3>");
+        $('#subwrapper').append("<h3>Incorrect Answers: "+this.incorrect+"</h3>");
+        $('#subwrapper').append("<h3>Unanswered: "+(questions.length -(this.incorrect+this.correct))+"</h3>");
     }
-}
 }
