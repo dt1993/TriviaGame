507118657ba3fbc5244e8ffcc5f29274ce28fa05
Eric Kinsley <dt1993@gmail.com>
2020-03-29 22:19:06 -0500

    update css

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -47,7 +47,7 @@ var game = {
     countdown: function(){
         game.counter--;
         $('#counter').html(game.counter);
-        if(game.counter==0){
+        if(game.counter<=0){
             console.log("Time is up!");
             game.done();
         }
@@ -55,7 +55,7 @@ var game = {
 
     start: function(){
         timer = setInterval(game.countdown,1000);
-        $('#subwrapper').prepend('<h2>Time Remaining: <span id="counter">120</span> Seconds</h2>')
+        $('#subwrapper').prepend('<h2 id="timer">Time Remaining: <span id="counter">120</span> Seconds</h2>')
         $("#start").remove();
         for(var i=0;i<questions.length;i++){
             $('#subwrapper').append('<h2>'+questions[i].question+'</h2');
